c4c43552ecc6b652c0dac0ecce7a54010a96c77f
JoshC <57299645+ArlieFuentson@users.noreply.github.com>
2020-12-19 11:27:48 -0500

    Day 3 complete

--- venv/day3.py
+++ venv/day3.py
@@ -31,4 +31,52 @@ def part1():
     data.close()
     print(trees)
 
+def part2():
+    #Store data in a list of lists // Matrix
+    grid = []
+    with open("../data/day3.csv", newline='') as data:
+        reader = csv.reader(data)
+        for line in reader:
+            temp = ""
+            for char in line:
+                temp += char
+            grid.append(temp)
+
+    #Iterate through data by various different methods as defined by the question, then multiply the number of trees
+    ans = 1
+    rightList = [1,3,5,7] #These are all right by ____ down by 1
+    for loop in rightList:
+        trees = 0
+        loc = 0
+        for i in range(len(grid)):
+            if (i == 0):
+                loc += loop
+                continue
+
+            if (grid[i][loc % len(grid[0])] == '#'):  #Mod divide because we want to go through the whole "mountain" of data on the toboggan
+               trees += 1
+
+            loc += loop
+
+        ans *= trees
+
+    #Now I need right by 1 down by 2
+    trees = 0
+    loc = 0
+    for i in range(0, len(grid), 2):
+        if (i == 0):
+            loc += 1
+            continue
+
+        if (grid[i][loc % len(grid[0])] == '#'):  # Mod divide because we want to go through the whole "mountain" of data on the toboggan
+            trees += 1
+
+        loc += 1
+
+    ans *= trees
+
+    print(ans)
+    data.close()
+
 part1()
+part2()
