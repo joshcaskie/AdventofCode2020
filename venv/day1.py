32773adfcadaeba8bddf2cee2181892419c607c9
JoshC <57299645+ArlieFuentson@users.noreply.github.com>
2020-12-18 23:35:06 -0500

    Reading lines properly now. Pulled in as List[String] so have to cast properly.

--- venv/day1.py
+++ venv/day1.py
@@ -2,14 +2,13 @@
 #https://www.w3schools.com/python/
 #https://docs.python.org/3/library/csv.html
 
-def day1():
-    file = open("..\data\day1.csv") #. is the current directory, and .. goes up one directory (talked about in 220 file i/o)
-    list: List[Int] = [10]
-    for i in file:
-        int(print(i))
+import csv
 
-    file.close()
-    print(list)
+def day1():
+    with open("../data/day1.csv", newline='') as data:
+        reader = csv.reader(data)
+        for line in reader:
+            print(line)
 
 
 day1()
