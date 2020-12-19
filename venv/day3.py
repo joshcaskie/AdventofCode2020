4982586f0d1d82c5baae92f9ef75ad53c3b9c9af
JoshC <57299645+ArlieFuentson@users.noreply.github.com>
2020-12-19 11:20:13 -0500

    Day 3 part 1 done

--- venv/day3.py
+++ venv/day3.py
@@ -12,18 +12,23 @@ def part1():
             grid.append(temp)
 
     #Iterate through data going 3 across and 1 down while checking for trees.
+    trees = 0
     loc = 0
     for i in range(len(grid)):
+        #How do I get python to "continue" on the loop // it is continuing - I just wasn't incrementing.
         if (i == 0):
+            loc += 3
             continue
-        elif (loc < len(grid[0])):
-            if (grid[i][loc] == '#'):
-               print("tree")
-            else:
-                print("clear")
+        #print(loc % len(grid[0]))
+        #print(grid[i][loc % len(grid[0])])
+        if (grid[i][loc % len(grid[0])] == '#'):  #Mod divide because we want to go through the whole "mountain" of data on the toboggan
+           trees += 1
+        #else:
+            #print("clear")
         loc += 3
 
     #print(grid)
     data.close()
+    print(trees)
 
 part1()
