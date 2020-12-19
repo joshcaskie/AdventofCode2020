6c1fc727b9f866a818fdb94ade069e7949fd3d5e
JoshC <57299645+ArlieFuentson@users.noreply.github.com>
2020-12-19 00:21:26 -0500

    Day 2 initial idea - troubleshooting

---
+++ venv/day2.py
@@ -0,0 +1,67 @@
+import csv
+
+#initial ideas are to check each line manually
+# 6-13 h: pghjchdxhnjhjd
+# Check for numbers up to the '-' and then check for numbers up to the first ' '. Then next letter 'x' and ': ' and then start checking the string
+# Is there a way to streamline this? Or do I have to do it manually?
+
+def part1():
+
+    numCorrect = 0
+
+    with open("../data/day2.csv", newline='') as data:
+        reader = csv.reader(data)
+        for line in reader:
+            length = len(line)
+            index = 0
+            firstNum = 0
+            secondNum = 0
+            ################################
+            #Get the first number
+            while (line[0][index] != '-'):
+                firstNum = firstNum * 10 + int(line[0][index])    #manually scanning the string for numbers and increasing accordingly by base 10
+                #print(int(line[index][0]))
+                index += 1
+
+            if (line[0][index] == '-'):
+                index += 1
+
+            #Second number
+            while (line[0][index] != ' '):
+                secondNum = secondNum * 10 + int(line[0][index])    #manually scanning the string for numbers and increasing accordingly by base 10
+                index += 1
+
+            if (line[0][index] == ' '):
+                index += 1
+
+            print(firstNum)
+            print(secondNum)
+
+            #Grab the character to search for
+            char = line[0][index]
+
+            print(char)
+
+            if(line[0][index] == ':' or line[0][index] == ' '):
+                index += 1
+
+            #Start looping through the rest of the list!
+
+            count = 0
+            while (index < length):
+                print(line[0][index])
+                if (line[0][index] == char):
+                    count += 1
+                index += 1
+
+
+
+            if (count >= firstNum and count <= secondNum):
+                numCorrect += 1
+        ####################################
+
+    data.close()
+
+    print(numCorrect)
+
+part1()
