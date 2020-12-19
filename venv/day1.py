ae0acd4cefd02ecdf4cd25c0e2f62a0e6e188d89
JoshC <57299645+ArlieFuentson@users.noreply.github.com>
2020-12-18 23:55:36 -0500

    Day 1 complete

--- venv/day1.py
+++ venv/day1.py
@@ -4,11 +4,52 @@
 
 import csv
 
-def day1():
+def getData():
+    list = []
+
     with open("../data/day1.csv", newline='') as data:
         reader = csv.reader(data)
         for line in reader:
-            print(line)
+            list.append(int(line[0]))
+        #print(list)
+    data.close()
+    return list
 
+def part1():
+    list = getData()
+    #Find two numbers that add to 2020, then print their multiplication
+    #O(n^2) runtime on the data set
+    length = len(list)
+    for i in range(length):
+        for j in range(length):
+            if i != j and (list[i] + list[j]) == 2020:
+                print("Part 1: " + str(list[i] * list[j]))
+                return
 
-day1()
+def part2():
+    list = getData()
+    #Find three numbers that add to 2020, then print their multiplication
+    #O(n^3)
+    length = len(list)
+    indices = range(length)
+    for i in indices:
+        for j in indices:
+            for k in indices:
+                if i != j and i != k and j != k and (list[i] + list[j] + list[k]) == 2020:
+                    print("Part 2: " + str(list[i] * list[j] * list[k]))
+                    return
+
+part1()
+part2()
+
+#Notes:
+#Current directory is .   - A directory up is ..
+#Review of CSV files in Python
+#Look into "typed python" so as to say if types are working properly/warm me about issues? Kind of like Scala?
+
+#Two methods of looping over a list
+#1. for loop in list:
+#2. length = len(list)
+#   for index in range(length):      --> will give indices from 0 to length minus 1
+
+#Not sure if return is the proper way to end the function early?
