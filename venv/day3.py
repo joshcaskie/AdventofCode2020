e2f1be86361e4f52a384df36b6c740dbb8f46b98
JoshC <57299645+ArlieFuentson@users.noreply.github.com>
2020-12-19 11:11:03 -0500

    Getting closer, but I think the problem wants to read the WHOLE file (so increment by 3 and use a modulus to keep going!)

--- venv/day3.py
+++ venv/day3.py
@@ -1,6 +1,7 @@
 import csv
 
 def part1():
+    #Store data in a list of lists // Matrix
     grid = []
     with open("../data/day3.csv", newline='') as data:
         reader = csv.reader(data)
@@ -9,6 +10,18 @@ def part1():
             for char in line:
                 temp += char
             grid.append(temp)
+
+    #Iterate through data going 3 across and 1 down while checking for trees.
+    loc = 0
+    for i in range(len(grid)):
+        if (i == 0):
+            continue
+        elif (loc < len(grid[0])):
+            if (grid[i][loc] == '#'):
+               print("tree")
+            else:
+                print("clear")
+        loc += 3
 
     #print(grid)
     data.close()
